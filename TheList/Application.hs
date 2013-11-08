5488366ffcee62bf97fd13dca86bbab6309292b4
Brian Gapinski <bgapinski@wesleyan.edu>
2013-11-08 21:48:20 +0000

    Max fixed App.hs

--- TheList/Application.hs
+++ TheList/Application.hs
@@ -22,6 +22,7 @@ import System.Log.FastLogger (mkLogger)
 import Handler.Home
 import Handler.AllUsers
 import Handler.UserById
+import Handler.Rating
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
