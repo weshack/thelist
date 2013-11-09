29b9f47b7ffb6d437f0100c18f0deb8810d57bc5
Max Dietz <mdietz@wesleyan.edu>
2013-11-08 22:56:28 -0500

    added route for modifying transaction

--- TheList/Application.hs
+++ TheList/Application.hs
@@ -28,6 +28,7 @@ import Handler.AllTrans
 import Handler.AuthCurrentId
 import Handler.TransById
 import Handler.AddTransaction
+import Handler.EditTransaction
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ TheList/Handler/EditTransaction.hs
@@ -0,0 +1,32 @@
+module Handler.EditTransaction where
+
+import Import
+import Data.Maybe (fromJust)
+import Yesod.Auth (requireAuthId)
+
+postEditTransactionR :: Handler Value
+postEditTransactionR = do
+    logged_in <- requireAuthId
+    ((result,_),_) <- runFormPost transactionForm
+    case result of
+        FormSuccess (PartialTransaction tId item description min_offer) -> do
+            t <- runDB $ get404 tId
+            case (transactionVendor t) of
+                logged_in -> do
+                    runDB $ update tId [TransactionItem =. item, TransactionBestOffer =. min_offer ]
+                    returnJson [ "result" .= ("ok" :: Text) ]
+                _ -> returnJson [ "result" .= ("error" :: Text) ]
+        _ -> returnJson [ "result" .= ("error" :: Text) ]
+
+data PartialTransaction = PartialTransaction TransactionId Text Text Text
+
+transactionForm :: Html -> MForm Handler (FormResult PartialTransaction, Widget)
+transactionForm = renderTable transactionAForm
+
+transactionAForm :: AForm Handler PartialTransaction
+transactionAForm = PartialTransaction
+    <$> ((fromJust . fromPathPiece) <$> areq textField "Transaction Id" Nothing)
+    <*> areq textField "Item" Nothing
+    <*> areq textField "Description" Nothing
+    <*> areq textField "Minimum Offer" Nothing
+
