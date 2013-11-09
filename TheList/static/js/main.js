8c58450b5639934d697c4cb29d12e5d893abe2c8
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2013-11-09 00:05:13 -0500

    more search, logged in

--- TheList/static/js/main.js
+++ TheList/static/js/main.js
@@ -1,8 +1,23 @@
+var isLoggedIn = function(){
+	$.getJSON("/current-user").done(function(response){
+		if (response['value'])
+			return false;
+		else
+			$("#accountform").html("<button  type='submit' class='btn btn-default navbar-btn'>" + response['value']['userName']+"</button>");
+	});
+};
 var searchTransactions = function(){
-	$.getJSON("/transactions/search").done(function(response){
+	query = $("#search-text").val();
+	$.getJSON("/transactions/search/" + query).done(function(response){
 		for (var i = 0; i < response.length; i++){
 				$("#search-results").insert("<div class='col-sm-6 col-md-3'><h2>" + response[i]['val']['item'] + "</h2><p>" + response[i]['val']['description'] + "</p></br>" + response[i]['val']['vendor'] + "</div>");
 		}
 		});
 	};
-}
+
+
+
+$(function(){
+	isLoggedIn();
+		
+});
