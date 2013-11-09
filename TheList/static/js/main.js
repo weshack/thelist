36740f883e5f4f1fb31685cea2849671f0fd45bc
Justin Raymond <justin.raymond.14@gmail.com>
2013-11-09 01:45:30 -0500

    post transaction form modal

--- TheList/static/js/main.js
+++ TheList/static/js/main.js
@@ -1,3 +1,4 @@
+/* Checks if user is logged in */
 var isLoggedIn = function(){
 	$.getJSON("/current-user").done(function(response){
 		if (!response['value'])
@@ -6,19 +7,18 @@ var isLoggedIn = function(){
       $("#accountform").html('<div class="dropdown-toggle" id="username" data-toggle="dropdown">' + response['value']['userIdent']+'</div><ul class="dropdown-menu"><li><a href="/transactions">My Transactions</a></li><li><a href="/offers">My Offers</a></li><li><a href="/settings">Settings</a></li><li><a href="/auth/logout">Log Out</a></li></ul>');
       });
 };
-
+/* searches transactions */
 var searchTransactions = function(){
 	query = $("#search-text").val();
 	$.getJSON("/transactions/search/" + query).done(function(response){
+    console.log(response);
 		for (var i = 0; i < response.length; i++){
-				$("#search-results").insert("<div class='col-sm-6 col-md-3'><h2>" + response[i]['val']['item'] + "</h2><p>" + response[i]['val']['description'] + "</p></br>" + response[i]['val']['vendor'] + "</div>");
+				$("#search-results").append("<div class='col-sm-6 col-md-3'><h2>" + 
+                                    response[i]['val']['item'] + "</h2><p>" + 
+                                    response[i]['val']['description'] + "</p></br>" + 
+                                    response[i]['val']['vendor'] + "</div>");
 		}
-		});
-	};
+  });
+};
 
 
-
-$(function(){
-	isLoggedIn();
-		
-});
