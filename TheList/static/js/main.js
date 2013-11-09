10b4e752d50382defa08dcb6c2c904388c1ccf11
Brian Gapinski <bgapinski@wesleyan.edu>
2013-11-09 17:05:11 +0000

    logging

--- TheList/static/js/main.js
+++ TheList/static/js/main.js
@@ -76,7 +76,7 @@ var showOffers = function(transaction) {
       for (var i=0; (i < response.length && i < 17); i++) {
         (function(r) {
           var price = $("<div>").text(r.value.offerOffer);
-          var btn = $("<button>").addClass('btn').addClass('transactionBtn').addClass('btn-default').text('Accept Offer').click(function(){acceptOffer(r);});
+          var btn = $("<button>").addClass('btn').addClass('transactionBtn').addClass('btn-default').text('Accept Offer').one('click', function(){acceptOffer(r);});
           price.append(btn);
           var li = $("<li>").addClass('offer').append(price);
           $("#"+transaction.key).append(li);
@@ -87,9 +87,11 @@ var showOffers = function(transaction) {
 };
 
 var acceptOffer = function(offer) {
-  console.log(offer);
-  $.getJSON("/offers/accept/" + offer.key).done(function() {
+  console.log("Before the accept");
+  $.getJSON("/offers/accept/" + offer.key).always(function() {
+    console.log("The accept callback");
     $("#myTransactionModal").modal("hide");
+    myTransactions();
     initSearch();
   });
 };
