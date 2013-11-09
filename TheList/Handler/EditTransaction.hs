eb9d93a04e21c1ece2d909b7b4797f88f5a000d6
Max Dietz <mdietz@wesleyan.edu>
2013-11-09 02:41:41 -0500

    fixed post forms

--- TheList/Handler/EditTransaction.hs
+++ TheList/Handler/EditTransaction.hs
@@ -7,26 +7,17 @@ import Yesod.Auth (requireAuthId)
 postEditTransactionR :: Handler Value
 postEditTransactionR = do
     logged_in <- requireAuthId
-    ((result,_),_) <- runFormPost transactionForm
-    case result of
-        FormSuccess (PartialTransaction tId item description min_offer) -> do
-            t <- runDB $ get404 tId
-            case ((transactionVendor t) == logged_in) of
-                True -> do
-                    runDB $ update tId [TransactionItem =. item, TransactionBestOffer =. min_offer, TransactionDescription =. description ]
-                    returnJson $ object [ "result" .= ("ok" :: Text) ]
-                False -> returnJson $ object [ "result" .= ("error" :: Text) ]
-        _ -> return $ object [ "result" .= ("error" :: Text) ]
+    (PartialTransaction tId item description min_offer) <- runInputPost $ PartialTransaction
+        <$> ((fromJust . fromPathPiece) <$> ireq textField "Transaction Id")
+        <*> ireq textField "Item" 
+        <*> ireq textField "Description" 
+        <*> ireq textField "Minimum Offer" 
+    t <- runDB $ get404 tId
+    case ((transactionVendor t) == logged_in) of
+        True -> do
+            runDB $ update tId [TransactionItem =. item, TransactionBestOffer =. min_offer, TransactionDescription =. description ]
+            returnJson $ object [ "result" .= ("ok" :: Text) ]
+        False -> returnJson $ object [ "result" .= ("error" :: Text) ]
 
 data PartialTransaction = PartialTransaction TransactionId Text Text Text
-
-transactionForm :: Html -> MForm Handler (FormResult PartialTransaction, Widget)
-transactionForm = renderTable transactionAForm
-
-transactionAForm :: AForm Handler PartialTransaction
-transactionAForm = PartialTransaction
-    <$> ((fromJust . fromPathPiece) <$> areq textField "Transaction Id" Nothing)
-    <*> areq textField "Item" Nothing
-    <*> areq textField "Description" Nothing
-    <*> areq textField "Minimum Offer" Nothing
 
