f201711eb0c860fd0ee209d5c6b134b40cb86207
Max Dietz <mdietz@wesleyan.edu>
2013-11-08 22:47:39 -0500

    Added route for adding new transactions

--- TheList/Application.hs
+++ TheList/Application.hs
@@ -27,6 +27,7 @@ import Handler.Offers
 import Handler.AllTrans
 import Handler.AuthCurrentId
 import Handler.TransById
+import Handler.AddTransaction
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ TheList/Handler/AddTransaction.hs
@@ -0,0 +1,27 @@
+module Handler.AddTransaction where
+
+import Import
+import Yesod.Auth (requireAuthId)
+import Data.Maybe (fromJust)
+
+postAddTransactionR :: Handler Value
+postAddTransactionR = do
+    logged_in <- requireAuthId
+    ((result,_),_) <- runFormPost transactionForm
+    case result of
+        FormSuccess (PartialTransaction item description min_offer) -> do
+                tId <- runDB $ insert (Transaction logged_in Nothing item  min_offer Nothing)
+                returnJson [ "transaction_id" .= tId ]
+        _ -> returnJson [ "result" .= ("error" :: Text) ]
+
+data PartialTransaction = PartialTransaction Text Text Text
+
+transactionForm :: Html -> MForm Handler (FormResult PartialTransaction, Widget)
+transactionForm = renderTable transactionAForm
+
+transactionAForm :: AForm Handler PartialTransaction
+transactionAForm = PartialTransaction
+    <$> areq textField "Item" Nothing
+    <*> areq textField "Description" Nothing
+    <*> areq textField "Minimum Offer" Nothing
+
